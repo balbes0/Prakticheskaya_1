add1aec5226ea407db4274e39684e637401acca5
balbes0 <ff347@mail.ru>
2024-02-02 15:15:52 +0300

    dop

--- Praktos1/MainWindow.xaml.cs
+++ Praktos1/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -19,92 +21,74 @@ namespace Praktos1
 {
     public partial class MainWindow : Window
     {
-        int randomnum;
-        string NameButton;
-        int GetNum;
-        char GetChar;
+        int RandomIndex;
         Random random = new Random();
-        string[] AvailableButtons = new string[9] { "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9" };
-        int countDisabled;
-        int CoutBotWins = 0;
-        int CoutUserWins = 0;
-        int CoutDraws = 0;
+        Button[] Buttons;
+        int countDisabled = 0;
+        int CountBotWins = 0;
+        int CountUserWins = 0;
+        int CountDraws = 0;
         bool flag;
+        int X_or_0 = 0;
+        string UserXili0;
+        string BotXili0;
 
         public MainWindow()
         {
             InitializeComponent();
+            Buttons = new Button[9] { B1, B2, B3, B4, B5, B6, B7, B8, B9 };
         }
 
         public void Botik()
         {
             if (flag == false)
             {
-                randomnum = random.Next(0, 8);
+                RandomIndex = random.Next(0, 8);
 
-                while (AvailableButtons[randomnum] == "disabled")
+                while (Buttons[RandomIndex].IsEnabled == false)
                 {
-                    randomnum = random.Next(0, 8);
+                    RandomIndex = random.Next(0, 8);
                 }
-
-                switch (randomnum)
+                if (X_or_0%2 == 0) 
                 {
-                    case 0:
-                        B1.Content = "0"; B1.IsEnabled = false; AvailableButtons[0] = "disabled";
-                        break;
-                    case 1:
-                        B2.Content = "0"; B2.IsEnabled = false; AvailableButtons[1] = "disabled";
-                        break;
-                    case 2:
-                        B3.Content = "0"; B3.IsEnabled = false; AvailableButtons[2] = "disabled";
-                        break;
-                    case 3:
-                        B4.Content = "0"; B4.IsEnabled = false; AvailableButtons[3] = "disabled";
-                        break;
-                    case 4:
-                        B5.Content = "0"; B5.IsEnabled = false; AvailableButtons[4] = "disabled";
-                        break;
-                    case 5:
-                        B6.Content = "0"; B6.IsEnabled = false; AvailableButtons[5] = "disabled";
-                        break;
-                    case 6:
-                        B7.Content = "0"; B7.IsEnabled = false; AvailableButtons[6] = "disabled";
-                        break;
-                    case 7:
-                        B8.Content = "0"; B8.IsEnabled = false; AvailableButtons[7] = "disabled";
-                        break;
-                    case 8:
-                        B9.Content = "0"; B9.IsEnabled = false; AvailableButtons[8] = "disabled";
-                        break;
+                    Buttons[RandomIndex].IsEnabled = false; Buttons[RandomIndex].Content = "0";
+                }
+                else if (X_or_0%2 == 1)
+                {
+                    Buttons[RandomIndex].IsEnabled = false; Buttons[RandomIndex].Content = "X";
                 }
             }
             CheckBotWin();
         }
         public void ResetGame()
         {
-            for (int i = 0; i < AvailableButtons.Length; i++)
+            foreach(var item in Buttons)
             {
-                AvailableButtons[i] = "B" + (i+1).ToString();
+                item.IsEnabled = true; item.Content = "";
             }
-            B1.Content = ""; B1.IsEnabled = true; B2.Content = ""; B2.IsEnabled = true; 
-            B3.Content = ""; B3.IsEnabled = true; B4.Content = ""; B4.IsEnabled = true; 
-            B5.Content = ""; B5.IsEnabled = true; B6.Content = ""; B6.IsEnabled = true;
-            B7.Content = ""; B7.IsEnabled = true; B8.Content = ""; B8.IsEnabled = true;
-            B9.Content = ""; B9.IsEnabled = true;
+            X_or_0 += 1;
         }
         public void CheckUserWin()
         {
-            if (B1.Content == "X" && B2.Content == "X" && B3.Content == "X" ||
-                B4.Content == "X" && B5.Content == "X" && B6.Content == "X" ||
-                B7.Content == "X" && B8.Content == "X" && B9.Content == "X" ||
-                B1.Content == "X" && B5.Content == "X" && B9.Content == "X" ||
-                B3.Content == "X" && B5.Content == "X" && B7.Content == "X" ||
-                B1.Content == "X" && B4.Content == "X" && B7.Content == "X" ||
-                B2.Content == "X" && B5.Content == "X" && B8.Content == "X" ||
-                B3.Content == "X" && B6.Content == "X" && B9.Content == "X")
+            if (X_or_0 % 2 == 0)
             {
-                CoutUserWins += 1;
-                UserWins.Text = "Кол-во ваших побед: " + CoutUserWins.ToString();
+                UserXili0 = "X";
+            }
+            else
+            {
+                UserXili0 = "0";
+            }
+            if (B1.Content == UserXili0 && B2.Content == UserXili0 && B3.Content == UserXili0 ||
+                B4.Content == UserXili0 && B5.Content == UserXili0 && B6.Content == UserXili0 ||
+                B7.Content == UserXili0 && B8.Content == UserXili0 && B9.Content == UserXili0 ||
+                B1.Content == UserXili0 && B5.Content == UserXili0 && B9.Content == UserXili0 ||
+                B3.Content == UserXili0 && B5.Content == UserXili0 && B7.Content == UserXili0 ||
+                B1.Content == UserXili0 && B4.Content == UserXili0 && B7.Content == UserXili0 ||
+                B2.Content == UserXili0 && B5.Content == UserXili0 && B8.Content == UserXili0 ||
+                B3.Content == UserXili0 && B6.Content == UserXili0 && B9.Content == UserXili0)
+            {
+                CountUserWins += 1;
+                UserWins.Text = "Кол-во ваших побед: " + CountUserWins.ToString();
                 MessageBox.Show("Выйграли вы");
                 flag = true;
                 ResetGame();
@@ -112,17 +96,25 @@ namespace Praktos1
         }
         public void CheckBotWin()
         {
-            if (B1.Content == "0" && B2.Content == "0" && B3.Content == "0" ||
-                B4.Content == "0" && B5.Content == "0" && B6.Content == "0" ||
-                B7.Content == "0" && B8.Content == "0" && B9.Content == "0" ||
-                B1.Content == "0" && B5.Content == "0" && B9.Content == "0" ||
-                B3.Content == "0" && B5.Content == "0" && B7.Content == "0" ||
-                B1.Content == "0" && B4.Content == "0" && B7.Content == "0" ||
-                B2.Content == "0" && B5.Content == "0" && B8.Content == "0" ||
-                B3.Content == "0" && B6.Content == "0" && B9.Content == "0")
+            if (X_or_0 % 2 == 0)
             {
-                CoutBotWins += 1;
-                BotWins.Text = "Кол-во побед бота: " + CoutBotWins.ToString();
+                BotXili0 = "0";
+            }
+            else
+            {
+                BotXili0 = "X";
+            }
+            if (B1.Content == BotXili0 && B2.Content == BotXili0 && B3.Content == BotXili0 ||
+                B4.Content == BotXili0 && B5.Content == BotXili0 && B6.Content == BotXili0 ||
+                B7.Content == BotXili0 && B8.Content == BotXili0 && B9.Content == BotXili0 ||
+                B1.Content == BotXili0 && B5.Content == BotXili0 && B9.Content == BotXili0 ||
+                B3.Content == BotXili0 && B5.Content == BotXili0 && B7.Content == BotXili0 ||
+                B1.Content == BotXili0 && B4.Content == BotXili0 && B7.Content == BotXili0 ||
+                B2.Content == BotXili0 && B5.Content == BotXili0 && B8.Content == BotXili0 ||
+                B3.Content == BotXili0 && B6.Content == BotXili0 && B9.Content == BotXili0)
+            {
+                CountBotWins += 1;
+                BotWins.Text = "Кол-во побед бота: " + CountBotWins.ToString();
                 MessageBox.Show("Выйграл ботик");
                 flag = true;
                 ResetGame();
@@ -130,43 +122,59 @@ namespace Praktos1
         }
         public void CheckDraw()
         {
-            countDisabled = AvailableButtons.Count(btn => btn == "disabled");
+            foreach (var item in Buttons) 
+            {
+                if (item.IsEnabled == false)
+                {
+                    countDisabled += 1;
+                }
+            }
             if (countDisabled == 9)
             {
-                CoutDraws += 1;
-                Draws.Text = "Кол-во ничьих: " + CoutDraws.ToString();
+                CountDraws += 1;
+                Draws.Text = "Кол-во ничьих: " + CountDraws.ToString();
                 MessageBox.Show("Ничья");
                 flag = true;
                 ResetGame();
             }
-        }
-        public void DisableButtons()
-        {
-            GetChar = NameButton[1];
-            GetNum = (int)Char.GetNumericValue(GetChar);
-            AvailableButtons[GetNum - 1] = "disabled";
+            else
+            {
+                countDisabled = 0;
+            }
         }
         private void RestartButton_Click(object sender, RoutedEventArgs e)
         {
-            CoutUserWins = 0;
-            UserWins.Text = "Кол-во ваших побед: " + CoutUserWins.ToString();
-            CoutBotWins = 0;
-            BotWins.Text = "Кол-во побед бота: " + CoutBotWins.ToString();
-            CoutDraws = 0;
-            Draws.Text = "Кол-во ничьих: " + CoutDraws.ToString();
+            CountUserWins = 0;
+            UserWins.Text = "Кол-во ваших побед: " + CountUserWins.ToString();
+            CountBotWins = 0;
+            BotWins.Text = "Кол-во побед бота: " + CountBotWins.ToString();
+            CountDraws = 0;
+            Draws.Text = "Кол-во ничьих: " + CountDraws.ToString();
             ResetGame();
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             flag = false;
-            (sender as Button).Content = "X";
-            (sender as Button).IsEnabled = false;
-            NameButton = (sender as Button).Name;
-            DisableButtons();
+            if (X_or_0 % 2 == 0)
+            {
+                (sender as Button).IsEnabled = false; (sender as Button).Content = "X";
+            }
+            else if (X_or_0 % 2 == 1)
+            {
+                (sender as Button).IsEnabled = false; (sender as Button).Content = "0";
+            }
             CheckUserWin();
             CheckDraw();
             Botik();
         }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            start_game.Visibility = Visibility.Hidden;
+            foreach(var item in Buttons)
+            {
+                item.IsEnabled = true;
+            }
+        }
     }
 }
